6b8ef220a16f75ca2c6b7b547df2457ea988af31
dasoya <66422476+dasoya@users.noreply.github.com>
2023-04-29 11:58:49 +0200

    Fix logical error and task 4-d
    change draw ,childWindowAtLocation : it should return deepst window so I change it recursively
    - hitTest : I changed it like this “return 0 <= x <= self.width and 0 <= y <= self.height” because it’s more safe.
    - bringWindowToFront : I read the given doc detaily and I found the windows brought to front are only top-level window. Before, any window can be brought to the top so I changed it.

--- Window.py
+++ Window.py
@@ -29,51 +29,89 @@ class Window:
         self.parentWindow.childWindows.remove(self)
         self.parentWindow = None
         
-    def childWindowAtLocation(self, x, y):
-        # if the window has no children, return None
-        topMostWindow = None
+    # def childWindowAtLocation(self, x, y):
+    #     # if the window has no children, return None
+    #     topMostWindow = None
 
-        if self.childWindows:
-            # iterate through child windows, from the bottom to the top
-            for childWindow in self.childWindows:
-                # check if position is within a given child window
-                # before, convert x and y to the local coordination system of the child window
-                if childWindow.hitTest(x - childWindow.x, y - childWindow.y):
-                    # if position is within a given child window, assign window to the variable
-                    topMostWindow = childWindow
-        # return a topmost found child window or None if no window exists             
-        return topMostWindow
+    #     if self.childWindows:
+    #         # iterate through child windows, from the bottom to the top
+    #         for childWindow in self.childWindows:
+    #             # check if position is within a given child window
+    #             # before, convert x and y to the local coordination system of the child window
+    #             if childWindow.hitTest(x - childWindow.x, y - childWindow.y):
+    #                 # if position is within a given child window, assign window to the variable
+    #                 topMostWindow = childWindow
     
+    #     # return a topmost found child window or None if no window exists             
+    #     return topMostWindow
+
+    
+    
+    def childWindowAtLocation(self, x, y):
+        # Check if the current window contains the provided point
+        if self.hitTest(x, y):
+            # Search for child windows in reverse order (topmost to bottommost)
+            for child in reversed(self.childWindows):
+                # Convert the local coordinates to the child window's coordinate system
+                childX = x - child.x
+                childY = y - child.y
+                # Recursively check child windows
+                result = child.childWindowAtLocation(childX, childY)
+                # Return the topmost child window found
+                if result is not None :
+                    return result 
+            # If no child window is found, return the current window
+            return self
+        else:
+            return None
+
     def hitTest(self, x, y):
         # if x and y are in a local coordinate system
         # it's sufficient to check if x and y are within bounds 
         # [0, width] and [0, height] of the current window 
-        return x <= self.width and y <= self.height
+        return 0 <= x <= self.width and 0 <= y <= self.height
     
     def convertPositionToScreen(self, x, y):
-        if self.parentWindow is not None:
-            # If this window has a parent, recursively convert local coordinates to screen coordinates
-            screenX, screenY = self.parentWindow.convertPositionToScreen(self.x, self.y)
-            return screenX + x, screenY + y
-        else:
-            # If this window has no parent, it is already at global screen coordinates
-            return self.x + x, self.y + y
+        localX = x
+        localY = y
+
+        # If this window has no parent, it is already at global screen coordinates
+        if self.parentWindow is None :
+            return localX, localY
+        
+        # If this window has a parent, recursively convert local coordinates to screen coordinates
+        else :
+            parentX = localX+ self.x 
+            parentY = localY+ self.y
+            return self.parentWindow.convertPositionToScreen(parentX,parentY) 
     
     def convertPositionFromScreen(self, x, y):
-        if self.parentWindow is not None:
-            # If this window has a parent, convert screen coordinates to parent coordinates
-            parentX, parentY = self.parentWindow.convertPositionFromScreen(x, y)
-            return parentX - self.x, parentY - self.y
-        else:
-            # If this window has no parent, it is already at global screen coordinates, so just subtract its position from the screen coordinates
-            return x - self.x, y - self.y
+
+        if self.parentWindow is None:
+            return (x, y)
+        
+        else :
+            
+            localX = x - self.x
+            localY = y - self.y
+            
+            return self.parentWindow.convertPositionFromScreen(localX, localY)
+        
     
     
     def draw(self, ctx):
-         # Draw the window's background color
+        # Draw the window's background color
         ctx.setFillColor(self.backgroundColor)
-        ctx.fillRect(self.x,self.y,self.x+ self.width, self.y+self.height)
+
+        if self.parentWindow is not None : 
+            ScreenX, ScreenY = self.parentWindow.convertPositionToScreen(self.x,self.y)
+        else :
+            ScreenX = self.x
+            ScreenY = self.y
         
+        ctx.setOrigin(ScreenX,ScreenY)
+        ctx.fillRect(0,0,self.width, self.height)
+
         # Draw any child windows
         for child in self.childWindows:
             child.draw(ctx)
--- WindowSystem.py
+++ WindowSystem.py
@@ -15,8 +15,27 @@ class WindowSystem(GraphicsEventSystem):
     def start(self):
         self.screen = Screen(self)
        
-        s2 = self.createWindowOnScreen(10,10,100,100,"SCREEN_2")
+        s2 = self.createWindowOnScreen(10,10,200,200,"SCREEN_2")
         s2.backgroundColor = COLOR_BLUE    
+
+      
+        s3 = self.createWindowOnScreen(50,50,200,200,"SCREEN_3")
+        s3.backgroundColor = COLOR_YELLOW   
+
+
+        s3_1 = Window(0,0,50,50,"SCREEN_3-1")
+        s3.addChildWindow(s3_1)
+        s3_1.backgroundColor = COLOR_BLACK
+
+        
+        s3_2 = Window(10,10,50,50,"SCREEN_3-2")
+        s3.addChildWindow(s3_2)
+        s3_2.backgroundColor = COLOR_PINK
+        
+        x,y = s3.convertPositionFromScreen(75,75) 
+        print(x, y)
+
+    
     
     """
     WINDOW MANAGEMENT
@@ -27,14 +46,31 @@ class WindowSystem(GraphicsEventSystem):
         self.screen.addChildWindow(window)
         return window
 
+    # def bringWindowToFront(self, window):
+    #     currWindow = window
+    #     while currWindow not in self.screen.childWindows and currWindow != self.screen:
+    #         currWindow = currWindow.parentWindow
+
+    #     if currWindow != self.screen:
+    #         currWindow.removeFromParentWindow()
+    #         self.screen.addChildWindow(currWindow)
+
     def bringWindowToFront(self, window):
         currWindow = window
-        while currWindow not in self.screen.childWindows and currWindow != self.screen:
-            currWindow = currWindow.parentWindow
-
-        if currWindow != self.screen:
+        # Check if the window is a top-level window
+        if window.parentWindow == self.screen:
             currWindow.removeFromParentWindow()
             self.screen.addChildWindow(currWindow)
+
+        elif window != self.screen:
+            # Find the top-level parent window
+            topParent = window
+            while topParent.parentWindow != self.screen:
+                topParent = topParent.parentWindow
+            
+            # Move the top-level parent window to the front
+            topParent.removeFromParentWindow()
+            self.screen.addChildWindow(topParent)
 
     """
     DRAWING
@@ -42,9 +78,6 @@ class WindowSystem(GraphicsEventSystem):
 
     def handlePaint(self):
         
-        #self.graphicsContext.setFillColor(COLOR_WHITE)
-        #self.graphicsContext.fillRect(0, 0, self.width, self.height)
-
         # Draw the screen and all its child windows
         self.screen.draw(self.graphicsContext)
         
@@ -54,21 +87,33 @@ class WindowSystem(GraphicsEventSystem):
     """
 
     def handleMousePressed(self, x, y):
-        # check the window at the given location
-        windowPressed = self.screen.childWindowAtLocation(x, y)
-        # if there exists a window in a given location
-        if windowPressed:
-            # bring it to the front
-            self.bringWindowToFront(windowPressed)
+    # Store the current mouse press coordinates
+        self.mousePressX = x
+        self.mousePressY = y
 
     def handleMouseReleased(self, x, y):
-        pass
+        # Check if the mouse release coordinates match the mouse press coordinates
+        if x == self.mousePressX and y == self.mousePressY:
+            # Check the window at the given location
+            windowClicked = self.screen.childWindowAtLocation(x, y)
+            # If there is a window at the location
+            if windowClicked:
+                # Bring it to the front
+                self.bringWindowToFront(windowClicked)
+                self.requestRepaint( )
+                # Propagate the click event to the window
+                windowClicked.handleMouseClicked(x, y)
+            else :
+                self.screen.handleMouseClicked(x,y)
+            
 
     def handleMouseMoved(self, x, y):
         pass
 
     def handleMouseDragged(self, x, y):
-        pass
+        # Clear the mouse press coordinates when mouse is dragged
+        self.mousePressX = None
+        self.mousePressY = None
 
     def handleKeyPressed(self, char):
         pass
