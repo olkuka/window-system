f97caa6d2f45a8e1552d6625fd984662522cc41c
Aleksandra Kukawka <kukawkaa@gmail.com>
2023-05-01 19:17:02 +0200

    add WindowManager.py

---
+++ WindowManager.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Window System - Submission
+by  Student Name 1 (#999999)
+and Student Name 2 (#999999)
+"""
+
+from GraphicsEventSystem import *
+from Window import *
+
+class WindowManager:
+    def __init__(self, windowSystem):
+        self.windowSystem = windowSystem
+    
+    
+    def checkWindowPosition(self, window, x, y):
+        pass
+    
+    
+    def decorateWindow(self, window, ctx):
+        pass
+    
+    
+    def drawDesktop(self, ctx):
+        pass
+    
+    
+    def drawTaskbar(self, ctx):
+        pass
+        
