ac2b3aa09d911f39d9964ddc2f107264a3d6de24
Aleksandra Kukawka <kukawkaa@gmail.com>
2023-05-02 22:35:43 +0200

    finish task 2

--- WindowManager.py
+++ WindowManager.py
@@ -13,14 +13,39 @@ from Window import *
 class WindowManager:
     def __init__(self, windowSystem):
         self.windowSystem = windowSystem
-        self.wallpaperColor = COLOR_LIGHT_BLUE
+        self.wallpaperColor = COLOR_PURPLE
+        self.titleBarHeight = 20
     
     def checkWindowPosition(self, window, x, y):
         pass
     
     
     def decorateWindow(self, window, ctx):
-        pass
+        # check if a window's a top-level window by checking if its parent window is the screen
+        if window.parentWindow == self.windowSystem.screen:
+            # add a title bar and set a different color if it's the foreground window
+            # (last on the screen's children list)
+            if window == self.windowSystem.screen.childWindows[-1]:
+                titleBarColor = COLOR_BLUE
+            else:
+                titleBarColor = COLOR_LIGHT_BLUE
+            ctx.setFillColor(titleBarColor)
+            ctx.fillRect(0, 0, window.width, self.titleBarHeight)
+            ctx.drawString(window.identifier, 4, 2)
+            
+            # add a stroked border
+            ctx.setStrokeColor(COLOR_LIGHT_GRAY)
+            ctx.strokeRect(0, 0, window.width, window.height)
+
+            # add a close button
+            ctx.drawLine(window.width - 15, 5, window.width - 5, 15)
+            ctx.drawLine(window.width - 15, 15, window.width - 5, 5)
+
+            # add a maximize button
+            ctx.strokeRect(window.width - 30, 5, window.width - 20, 15)
+
+            # add a minimize button
+            ctx.drawLine(window.width - 45, 10, window.width - 35, 10)
     
     
     def drawDesktop(self, ctx):
