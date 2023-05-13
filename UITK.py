8665d1cc641667f8f5bbd47905f6958e4ab7a1f0
Aleksandra Kukawka <48186382+olkuka@users.noreply.github.com>
2023-05-13 19:36:45 +0200

    P4 task 3 - Container Widgets (#7)
    * container
    
    * add comments

--- UITK.py
+++ UITK.py
@@ -11,80 +11,115 @@ from GraphicsEventSystem import *
 from Window import *
 import enum
 
+
 class Widget(Window):
-	def __init__(self, originX, originY, width, height, identifier):
-		super().__init__(originX, originY, width, height, identifier)
-		self.backgroundColor = COLOR_CLEAR
-
-
+    def __init__(self, originX, originY, width, height, identifier):
+        super().__init__(originX, originY, width, height, identifier)
+        self.backgroundColor = COLOR_CLEAR
 
 
 class Container(Widget):
-	def resize(self, x, y, width, height):
-		super().resize(x, y, width, height)
+    def __init__(self, originX, originY, width, height, identifier, axis='horizontal', spacing=0):
+        super().__init__(originX, originY, width, height, identifier)
+        self.axis = axis
+        self.spacing = spacing
+        self.children = []
 
+    # resize the container
+    def resize(self, x, y, width, height):
+        super().resize(x, y, width, height)
+        self.layoutChildren()
 
+    # add a new child
+    def addChild(self, child):
+        self.children.append(child)
+
+    # remove a child
+    def removeChild(self, child):
+        self.children.remove(child)
+        self.layoutChildren()
+
+    def layoutChildren(self):
+        if self.axis == 'horizontal':
+            # calculate the total width of the container taking its children and spacing into account
+            totalWidth = sum([child.width for child in self.children]
+                             ) + self.spacing * (len(self.children) - 1)
+            # set a new x coordinate
+            x = self.originX + (self.width - totalWidth) // 2
+            y = self.originY
+
+            # resize each children recursively and take care of the spacing
+            for child in self.children:
+                child.resize(x, y, child.width, child.height)
+                x += child.width + self.spacing
+
+        elif self.axis == 'vertical':
+            # calculate the total height of the container taking its children and spacing into account
+            totalHeight = sum([child.height for child in self.children]
+                              ) + self.spacing * (len(self.children) - 1)
+            x = self.originX
+            # set a new y coordinate
+            y = self.originY + (self.height - totalHeight) // 2
+            # resize each children recursively and take care of the spacing
+            for child in self.children:
+                child.resize(x, y, child.width, child.height)
+                y += child.height + self.spacing
 
 
 class Label(Widget):
-	def __init__(self, originX, originY, width, height, identifier,text,backgroundColor):
-		super().__init__(originX, originY, width, height, identifier)
-		
-		self.normalColor = backgroundColor
-		self.backgroundColor = self.normalColor
-		self.text = text
+    def __init__(self, originX, originY, width, height, identifier, text, backgroundColor):
+        super().__init__(originX, originY, width, height, identifier)
 
-	def draw(self, ctx):
-		super().draw(ctx)
-		ctx.setFont(None)
-		ctx.setStrokeColor(COLOR_BLACK)
-		ctx.drawString(self.text,0,0)
-		
+        self.normalColor = backgroundColor
+        self.backgroundColor = self.normalColor
+        self.text = text
+
+    def draw(self, ctx):
+        super().draw(ctx)
+        ctx.setFont(None)
+        ctx.setStrokeColor(COLOR_BLACK)
+        ctx.drawString(self.text, 0, 0)
+
 
 class Button(Label):
 
-	def __init__(self, originX, originY, width, height, identifier,text,backgroundColor,action):
-		super().__init__(originX, originY, width, height, identifier,text,backgroundColor)
-		self.backgroundColor = backgroundColor
-		self.action = action
-		self.BtnState = BtnState.Normal
+    def __init__(self, originX, originY, width, height, identifier, text, backgroundColor, action):
+        super().__init__(originX, originY, width, height, identifier, text, backgroundColor)
+        self.backgroundColor = backgroundColor
+        self.action = action
+        self.BtnState = BtnState.Normal
 
-	def draw(self, ctx):
-		if self.BtnState == BtnState.Normal:
-			self.backgroundColor = self.normalColor
-		if self.BtnState == BtnState.Hovering:
-			self.backgroundColor = COLOR_BLUE
-		if self.BtnState == BtnState.Pressed:
-			self.backgroundColor = COLOR_DARK_BLUE
-			
-		super().draw(ctx)
+    def draw(self, ctx):
+        if self.BtnState == BtnState.Normal:
+            self.backgroundColor = self.normalColor
+        if self.BtnState == BtnState.Hovering:
+            self.backgroundColor = COLOR_BLUE
+        if self.BtnState == BtnState.Pressed:
+            self.backgroundColor = COLOR_DARK_BLUE
 
-		# draw a button's frame
-		ctx.setStrokeColor(COLOR_WHITE)
-		ctx.drawLine(0,0,self.width,0)
-		ctx.drawLine(0,0,0,self.height)
-			
-		ctx.setStrokeColor(COLOR_GRAY)
-		ctx.drawLine(self.width-1,0,self.width-1,self.height)
-		ctx.drawLine(0,self.height,self.width-1,self.height)
+        super().draw(ctx)
 
-		ctx.setStrokeColor(COLOR_BLACK)
-		ctx.drawLine(self.width,0,self.width,self.height)
-		ctx.drawLine(0,self.height,self.width,self.height)
+        # draw a button's frame
+        ctx.setStrokeColor(COLOR_WHITE)
+        ctx.drawLine(0, 0, self.width, 0)
+        ctx.drawLine(0, 0, 0, self.height)
 
-		
+        ctx.setStrokeColor(COLOR_GRAY)
+        ctx.drawLine(self.width-1, 0, self.width-1, self.height)
+        ctx.drawLine(0, self.height, self.width-1, self.height)
 
-	
+        ctx.setStrokeColor(COLOR_BLACK)
+        ctx.drawLine(self.width, 0, self.width, self.height)
+        ctx.drawLine(0, self.height, self.width, self.height)
+
 
 # Using enum class to define the possible states of a button widget.
 class BtnState(enum.Enum):
-   Normal = 1
-   Hovering = 2
-   Pressed = 3
+    Normal = 1
+    Hovering = 2
+    Pressed = 3
+
 
 class Slider(Widget):
-	def draw(self, ctx):
-		super().draw(ctx)
-		
-	
-		
+    def draw(self, ctx):
+        super().draw(ctx)
