9ed9123d89c84d6f68e37c317710dab4012b2d54
dasoya <66422476+dasoya@users.noreply.github.com>
2023-05-19 13:14:41 +0200

    Update WindowSystem.py

--- WindowSystem.py
+++ WindowSystem.py
@@ -77,10 +77,10 @@ class WindowSystem(GraphicsEventSystem):
         # s4_5.backgroundColor = COLOR_BLACK
         # s4_5.layoutAnchors = LayoutAnchor.left
 
-        helloWorld = HelloWorld(100,100,400,400,"1")
+        helloWorld = HelloWorld(100,100,400,400)
         self.screen.addChildWindow(helloWorld)
         
-        cal = Calculator(200,200,300,500,"Calculator")
+        cal = Calculator(200,200,300,500)
         self.screen.addChildWindow(cal)
 
 
